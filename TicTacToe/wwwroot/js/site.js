9da0b823e2de0cb83625e145af562c9d46ccca99
ianwesleyarmstrong <ianwesleyarmstrong@F1092-40-2313>
2019-11-11 15:21:06 -0600

    some stuf

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,73 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var turn = "x";
+
+function setTurn() {
+    var turnElement = document.getElementById("turn");
+    turnElement.innerText = "it is player " + turn + "'s turn";
+}
+
+function setWinner() {
+    var turnElement = document.getElementById("turn");
+    turnElement.innerText = "Player " + turn + " has Won";
+    var cells = document.getElementsByClassName("square");
+    for (i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener("click", onClick);
+    }
+}
+
+function onClick(event) {
+    event.preventDefault();
+    if (!event.target.innerText) {
+        event.target.innerText = turn;
+        if (turn === "x") {
+            turn = "o";
+        }
+        else {
+            turn = "x";
+        }
+        if (checkForWin()) setTurn();
+    }
+}
+
+function checkForWin() {
+    var cells = document.getElementsByClassName("square");
+    // horizontal
+    for (var i = 0; i < 9; i += 3)
+    {
+        if (cells[i].innerText !== "" && cells[0 + i].innerText === cells[1 + i].innerText && cells[1 + i].innerText === cells[2 + i].innerText) {
+            setWinner(cells[0])
+            return true;
+        }
+    }
+    // vertical
+    for (var i = 0; i < 9; i += 3) {
+        if (cells[i].innerText !== "" && cells[0 + i].innerText === cells[3 + i].innerText && cells[3 + i].innerText === cells[6 + i].innerText) {
+            setWinner(cells[0])
+            return true;
+        }
+    }
+    // left diagonal
+    if (cells[i].innerText !== "" && cells[0].innerText === cells[4].innerText && cells[4].innerText === cells[8].innerText) {
+        setWinner(cells[0])
+        return true;
+    }
+
+    // right diagonal
+    if (cells[i].innerText !== "" && cells[2].innerText === cells[4].innerText && cells[4].innerText === cells[6].innerText) {
+        setWinner(cells[0])
+        return true
+    }
+    return false;
+}
+
+    
+    
+
+var cells = document.getElementsByClassName("square");
+for (i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", onClick);
+}
+
+setTurn();
